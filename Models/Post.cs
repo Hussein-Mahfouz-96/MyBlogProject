2bf82ba9dbff920e4264e3f7c6f5f513df98c78c
lora lameh <loralameh@gmail.com>
2021-07-30 00:08:12 +0300

    added the model for the project

---
+++ Models/Post.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace  FinalProject.Models{
+public class Post 
+{
+     public int PostId { get; set; }
+     public string UserName { get; set; }
+        public string Title  { get; set; }
+        
+        public string Content  { get; set; }
+        [BindProperty]
+        public DateTime  TimeStamp  { get; set; }
+
+
+
+}
+}
