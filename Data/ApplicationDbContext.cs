29b4a36eff2f3ff5d6a263b771bb737f2f007b28
lora lameh <loralameh@gmail.com>
2021-07-30 00:16:59 +0300

    added the Post model to the application data base context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyBlog.Models;
 
 namespace MyBlog.Data
 {
@@ -12,5 +13,7 @@ namespace MyBlog.Data
             : base(options)
         {
         }
+        public DbSet<Post> Posts { get; set; }
+
     }
 }
